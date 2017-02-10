10fa3931450f75ffb753ce2d04bbf9c9b43f71ad
Leo Moon <leomoondev@gmail.com>
2017-02-10 09:07:16 -0800

    Added Printer classes

---
+++ WordPrinter/Printer.h
@@ -0,0 +1,13 @@
+//
+//  Printer.h
+//  WordPrinter
+//
+//  Created by Hyung Jip Moon on 2017-02-10.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Printer : NSObject
+
+@end
---
+++ WordPrinter/Printer.m
@@ -0,0 +1,13 @@
+//
+//  Printer.m
+//  WordPrinter
+//
+//  Created by Hyung Jip Moon on 2017-02-10.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import "Printer.h"
+
+@implementation Printer
+
+@end
